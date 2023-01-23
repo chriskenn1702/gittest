b0db88c996f82afa25f4888f9221e23f647ea614
Christopher Kennedy <kennednu@bc.edu>
2023-01-23 14:24:49 -0500

    Initial Commit

---
+++ gittest/gittestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gittestApp.swift
+//  gittest
+//
+//  Created by Christopher Kennedy on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct gittestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
